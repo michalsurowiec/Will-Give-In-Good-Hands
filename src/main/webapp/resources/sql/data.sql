1ec209e93bcbac9dfe11bb01d7c2efdf70c1c308
michalsurowiec <michalsurowiec94@o2.pl>
2019-07-31 12:46:01 +0200

    completed all examples in data.sql

--- src/main/webapp/resources/sql/data.sql
+++ src/main/webapp/resources/sql/data.sql
@@ -1,3 +1,7 @@
 INSERT INTO category VALUES (null, 'Zabawki'), (null, 'Ubrania'), (null, 'Buty')
 
 INSERT INTO institution VALUES (null, 'Pomoc dzieciom z ubogich rodzin.', 'Dbam o Zdrowie'), (null, 'Pomoc wybudzaniu dzieci ze śpiączki.', 'A kogo'), (null, 'Pomoc osobom znajdującym się w trudnej sytuacji życiowej.', 'Dla dzieci'), (null, 'Pomoc dla osób nie posiadających miejsca zamieszkania', 'Bez domu')
+
+INSERT INTO donation VALUES (null, 'Wrocław', 'Po prostu to weźcie', '2019-07-19', '15:08', 1, 'Daszyńskiego', '50-156', 1), (null, 'Wałbrzych', 'Nara', '2019-07-18', '12:08', 2, 'Paderewskiego', '30-156', 2), (null, 'Inowrocław', 'Kai mosz bilet', '2019-07-17', '15:39', 1, 'Wyszyńskiego', '50-789', 3), (null, 'Kożuszki', 'Hasiok', '2019-07-16', '10:08', 2, 'Piłsudskiego', '19-156', 4), (null, 'Wrocław', 'Nudzi mi się', '2019-07-15', '9:09', 1, '3 Maja', '42-400', 1)
+
+INSERT INTO donation_categories VALUES (1, 1), (2, 2), (3, 3), (4, 1), (5, 2)
