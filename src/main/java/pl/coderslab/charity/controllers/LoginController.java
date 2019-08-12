9b7bf74c175e6d1725b6e1fed01724f6e688c249
michalsurowiec <michalsurowiec94@o2.pl>
2019-08-12 11:29:18 +0200

    deleted unused lines of code in LoginController

--- src/main/java/pl/coderslab/charity/controllers/LoginController.java
+++ src/main/java/pl/coderslab/charity/controllers/LoginController.java
@@ -4,10 +4,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,17 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginController
 {
     @GetMapping(value = "/login")
-    public String loginPage(@RequestParam(value = "error", required = false) String error,
-                            @RequestParam(value = "logout", required = false) String logout,
-                            Model model) {
-        String errorMessge = null;
-        if(error != null) {
-            errorMessge = "Username or Password is incorrect !!";
-        }
-        if(logout != null) {
-            errorMessge = "You have been successfully logged out !!";
-        }
-        model.addAttribute("errorMessge", errorMessge);
+    public String loginPage() {
         return "login";
     }
 
@@ -36,6 +23,6 @@ public class LoginController
         if (auth != null){
             new SecurityContextLogoutHandler().logout(request, response, auth);
         }
-        return "redirect:/login?logout=true";
+        return "redirect:/login";
     }
 }
