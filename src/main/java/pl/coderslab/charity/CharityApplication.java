2544a863079dcea62741c94d4c8ced088a5f51c0
michalsurowiec <michalsurowiec94@o2.pl>
2019-09-07 13:06:25 +0200

    created class to handle notifications

--- src/main/java/pl/coderslab/charity/CharityApplication.java
+++ src/main/java/pl/coderslab/charity/CharityApplication.java
@@ -2,6 +2,7 @@ package pl.coderslab.charity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CharityApplication {
@@ -9,5 +10,10 @@ public class CharityApplication {
     public static void main(String[] args) {
         SpringApplication.run(CharityApplication.class, args);
     }
+
+//    @Bean
+//    public NotificationCreator notificationCreator(){
+//        return new NotificationCreator();
+//    }
 
 }
